17b531f166475789c9ab6283966c1e76961f43e3
Jesus Zuniga <jesus.zuniga@macys.com>
2019-08-06 15:40:07 -0400

    This is the First commit

---
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,11 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount() {
+        System.out.println(getClass() + ": DOINT MY DB WORK: ADDING AN ACCOUNT");
+    }
+}
